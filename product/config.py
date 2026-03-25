3628ac14df7e7b2f6f65a4d42e7ecd899023b2fd
Sanidhya Karnik <karnik.sanidhya@gmail.com>
2026-03-24 22:35:40 -0400

    fix: ignore extra env vars in pydantic settings

--- product/config.py
+++ product/config.py
@@ -58,6 +58,7 @@ class Settings(BaseSettings):
         env_file = ".env"
         env_file_encoding = "utf-8"
         case_sensitive = False
+        extra = "ignore"  # Ignore extra env vars not defined in Settings
 
 
 def get_settings() -> Settings:
